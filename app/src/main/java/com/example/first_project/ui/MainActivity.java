be93e12c289e6ec9e1d4f0d4120f9395337f405a
usamafiaz-mm <u.fayyaz@pk.micromerger.com>
2022-09-09 04:38:35 +0500

    merged all tasks together

--- app/src/main/java/com/example/first_project/ui/MainActivity.java
+++ app/src/main/java/com/example/first_project/ui/MainActivity.java
@@ -1,32 +1,46 @@
 package com.example.first_project.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.example.first_project.data.Constant;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.first_project.R;
-import com.example.first_project.ui.adapters.StudentAdapter;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    Button timeOutButton, interceptButton,uiButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        ArrayList studentList = new Constant().STUDENTS;
-        StudentAdapter adapter = new StudentAdapter(studentList);
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
+        timeOutButton = findViewById(R.id.toBtn);
+        interceptButton = findViewById(R.id.interceptBtn);
+        uiButton = findViewById(R.id.UIBtn);
 
+        timeOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TimeOutActivity.class);
+                startActivity(intent);
 
+            }
+        });
+        interceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InterceptorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        uiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
